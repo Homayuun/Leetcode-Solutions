b862b3c22bede2b4d95308ee3320a5ae7a25436a
Homayun Gohari <30705391+Homayuun@users.noreply.github.com>
2025-07-15 22:49:45 +0330

    update problem 151

--- 151. Reverse Words in a String.swift
+++ 151. Reverse Words in a String.swift
@@ -3,21 +3,6 @@
 
 class Solution {
     func reverseWords(_ s: String) -> String {
-        var sArr = Array(s)
-        var result = [String]()
-        var left = 0
-        while left < s.count {
-            if sArr[left].isLetter || sArr[left].isNumber {
-                var right = left
-                while right < sArr.count && sArr[right] != " " {
-                    right += 1
-                }
-                var word = String(sArr[left..<right])
-                result.append(word)
-                left = right
-            }
-            left += 1
-        }
-        return result.reversed().joined(separator: " ")
+      return s.split(separator: " ").reversed().joined(separator: " ")
     }
 }
